b31e06131fca59d40cb38ed998a5650b07c064ca
jojoford <75343703+jojoford@users.noreply.github.com>
2021-03-10 20:15:48 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 // Include your MySQL user/password information
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('todolist', 'root', '', {
+  : new Sequelize('todolist', 'root', 'classpass', {
       host: 'localhost',
       port: 3306,
       dialect: 'mysql'
